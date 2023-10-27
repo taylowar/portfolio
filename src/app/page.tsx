4e1d4d481cf99586790c26fbab7284a14f8346c0
pwnker <tilen.okretic@gmail.com>
2023-10-28 01:10:11 +0200

    Fixed build problems

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { api } from "~/trpc/server";
 import Intro from "./_components/intro";
 import Devider from "./_components/devider";
 import About from "./_components/about";
 
-export default async function Home() {
-  const hello = await api.post.hello.query({ text: "from tRPC" });
+export default function Home() {
+//  const hello = await api.post.hello.query({ text: "from tRPC" });
 
   return (
     <main className="flex flex-col items-center px-4">
