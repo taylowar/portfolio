f9e0b3bf8dcd9d202cc4f1195754f397df4dac5c
pwnker <tilen.okretic@gmail.com>
2023-11-01 14:02:27 +0100

    Temporary solution to async fuunction handling

--- src/app/_lib/actions.ts
+++ src/app/_lib/actions.ts
@@ -3,4 +3,10 @@
 export const processEmail = async (formData: FormData) => {
     console.log(formData.get('email'));
     console.log(formData.get('message'));
+    await new Promise((resolve, _) => {
+        setTimeout(() => {
+            console.log('FU');
+            return resolve(true);
+        }, 1000)
+    });
 };
