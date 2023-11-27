73a17c720104f5c58694c79cac50aee29f01c82e
pwnker <tilen.okretic@gmail.com>
2023-11-27 15:05:43 +0100

    Removed the cheesy #

--- src/app/_components/project.tsx
+++ src/app/_components/project.tsx
@@ -74,7 +74,7 @@ export default function Project({
 	                   key={index}
 	                   className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full "
 	                   >
-	                   #{tag}
+	                   {tag}
 	                   </li>
 	               ))}
 	               </ul>
