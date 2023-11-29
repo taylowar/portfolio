72317239cc9d3a73a7a36896cf10e74a68d9a8ab
pwnker <tilen.okretic@gmail.com>
2023-11-29 13:14:09 +0100

    Fixed page.tsx with force-dynamic

--- src/app/si/page.tsx
+++ src/app/si/page.tsx
@@ -1,4 +1,7 @@
+export const dynamic = 'force-dynamic';
+
 import React from 'react';
+
 import Home from '../_components/sections/home';
 import Devider from '../_components/devider';
 import About from '../_components/sections/about';
@@ -8,12 +11,12 @@ import Experience from '../_components/sections/experience';
 import Contact from '../_components/sections/contact';
 import ParticleContainer from '../_components/ParticleContainer';
 import TranslationContextProvider from '../_context/translation-context';
-import { api } from '~/trpc/react';
+import { api } from '~/trpc/server';
 
-export default function Page() {
-    const { data: i18n } = api.translator.i18n.useQuery({lang: 'si'});
+export default async function Page() {
+    const i18n = await api.translator.i18n.query({lang: 'si'});
 
-    return (i18n &&
+    return (
         <TranslationContextProvider i18n={i18n}>
             <main className="flex flex-col items-center px-4">
                 <ParticleContainer />
